797b07064618170c99dc60b58e4b902d9a3a80ef
Lucrene <ladyydanielle250@gmail.com>
2026-06-25 11:46:20 +0000

    Modification des paramètres RandomForest

--- src/train.py
+++ src/train.py
@@ -1,34 +1,58 @@
+import mlflow
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from utils import load_data
+
+from utils import normalize_data
 
 # Charger dataset MNIST (digits)
-X, y = load_data()
+digits = load_digits()
+X = digits.data
+y = digits.target
 
 # Split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Modèle final (choix du groupe)
-model = RandomForestClassifier(
-    n_estimators=150,
-    max_depth=10,
-    random_state=42
-)
+# Normalisation (si utils fonctionne)
+X_train, X_test = normalize_data(X_train, X_test)
 
-# Training
-model.fit(X_train, y_train)
+# Paramètres du modèle
+n_estimators = 250
+max_depth = 30
+random_state = 60
 
-# Prediction
-y_pred = model.predict(X_test)
+mlflow.set_experiment("MiniProjet2")
 
-# Accuracy
-acc = accuracy_score(y_test, y_pred)
+with mlflow.start_run():
 
-print("===================================")
-print("FINAL MODEL - Random Forest")
-print("Accuracy:", acc)
-print("===================================")
+    # Log des paramètres
+    mlflow.log_param("n_estimators", n_estimators)
+    mlflow.log_param("max_depth", max_depth)
+    mlflow.log_param("random_state", random_state)
+
+    # Modèle
+    model = RandomForestClassifier(
+        n_estimators=n_estimators,
+        max_depth=max_depth,
+        random_state=random_state
+    )
+
+    # Training
+    model.fit(X_train, y_train)
+
+    # Prediction
+    y_pred = model.predict(X_test)
+
+    # Accuracy
+    acc = accuracy_score(y_test, y_pred)
+
+    # Log métrique
+    mlflow.log_metric("accuracy", acc)
+
+    print("===================================")
+    print("FINAL MODEL - Random Forest")
+    print("Accuracy:", acc)
+    print("===================================")
--- src/utils.py
+++ src/utils.py
@@ -1,22 +1,7 @@
-<<<<<<< HEAD
 from sklearn.preprocessing import StandardScaler
 
 def normalize_data(X_train, X_test):
     scaler = StandardScaler()
-
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
-
     return X_train, X_test
-=======
-# Fonctions utilitaires
-from sklearn.datasets import load_digits
-
-def load_data():
-    digits = load_digits()
-
-    X = digits.data      # images aplaties (8x8 → 64 features)
-    y = digits.target    # labels 0-9
-
-    return X, y
->>>>>>> feature-model-alpha
